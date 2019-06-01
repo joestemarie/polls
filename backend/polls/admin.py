8b62632af94d67774396c9588d2a14280a315640
= <jmarie@bpimedia.com>
2019-06-01 13:20:20 -0400

    Add models to Django admin

--- backend/polls/admin.py
+++ backend/polls/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Candidate, Question, Pollster, Poll, QuestionResult
 
 # Register your models here.
+admin.site.register(Candidate)
+admin.site.register(Question)
+admin.site.register(Pollster)
+admin.site.register(Poll)
+admin.site.register(QuestionResult)
